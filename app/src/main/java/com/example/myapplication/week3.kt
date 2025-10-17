f22756477fb419ed6c8f33887cb4246c049a61cd
Monosix8 <ridhohidayatullah008@gmail.com>
2025-10-17 16:46:55 +0700

    baris 1

--- app/src/main/java/com/example/myapplication/week3.kt
+++ app/src/main/java/com/example/myapplication/week3.kt
@@ -60,3 +60,14 @@ fun LatihanBox(modifier: Modifier) {
     }
 }
 
+@Composable
+fun LatihanLetakColumnRow(modifier: Modifier) {
+    Column {
+        // Baris 1
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris1")
+            Text(text = "Komponen2Baris1")
+            Text(text = "Komponen3Baris1")
+        }
+
