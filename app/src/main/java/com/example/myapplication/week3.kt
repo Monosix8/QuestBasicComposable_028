61bb448803cde458f27ec81aa32c4a4035e69940
Monosix8 <ridhohidayatullah008@gmail.com>
2025-10-17 16:47:21 +0700

    baris 2

--- app/src/main/java/com/example/myapplication/week3.kt
+++ app/src/main/java/com/example/myapplication/week3.kt
@@ -71,3 +71,13 @@ fun LatihanLetakColumnRow(modifier: Modifier) {
             Text(text = "Komponen3Baris1")
         }
 
+        // Baris 2
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Komponen1Baris2")
+            Text(text = "Komponen2Baris2")
+            Text(text = "Komponen3Baris2")
+        }
+    }
+}
+
